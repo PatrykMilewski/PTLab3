ae82cfb85a18bb3f671e03c527c310fc149ca517
PatrykMilewski <patryk.milewski@gmail.com>
2017-03-22 10:43:21 +0100

    Initial commit

---
+++ src/application/Controller.java
@@ -0,0 +1,33 @@
+package application;
+
+
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    void chooseFile() {
+
+    }
+
+    @FXML
+    void convertFile() {
+
+    }
+
+    @FXML
+    void sequentialSelect() {
+
+    }
+
+    @FXML
+    void poolSelect() {
+
+    }
+
+    @FXML
+    void customPoolSelect() {
+
+    }
+
+}
